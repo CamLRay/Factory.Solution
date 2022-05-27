ee46dc937686f1f1cb98413ea2a71b879b3b82cf
Sploob <crayiphone@gmail.com>
2022-05-27 09:47:45 -0700

    add intial files and machine and engineer models

---
+++ Models/Engineer.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<MachineEngineer>();
+        }
+
+        public int EngineerId { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime StartDate { get; set; }
+        
+
+        public virtual ICollection<MachineEngineer> JoinEntities { get;}
+    }
+}
